2a0946098b5bf782bed755c80923d24570abb5f9
Mayank Mandava <mayankmandava@gmail.com>
2013-12-21 02:02:38 +0530

    Initial commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject clj-time-series "0.1.0-SNAPSHOT"
+  :description "A clojure Library for handling time series data"
+  :url "https://github.com/xmonkee/clj-time-series"
+  :license {:name "MIT License"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+  		[incanter "1.5.4"]
+		[clj-time "0.6.0"]
+		[org.clojure/data.csv "0.1.2"]
+		[clj-quandl "0.1.0-SNAPSHOT"]])
---
+++ src/clj_time_series/readfile.clj
@@ -0,0 +1,34 @@
+(ns clj-time-series.readfile
+  (:require 
+  	[clojure.java.io :as io]
+  	[clj-time.format :as fmt]))
+
+(def ^:dynamic *filepath* "data/")
+
+(def ddmmmyyyy (fmt/formatter "dd-MMM-yy"))
+
+(defn readline 
+	"parses time series data into clj-time date and long"
+	[line] 
+	(let [[rawdate & rawprices] (clojure.string/split line #",")]
+		(into [(fmt/parse ddmmmyyyy rawdate)]
+			(map #(Float/parseFloat %) rawprices))))
+
+(defn readheaders
+	"reads the first line as headers and changes to keywords"
+	[headerline]
+	(let [headersraw (clojure.string/split headerline #",")]
+		(conj 
+			(map #(keyword (.toLowerCase %)) (rest headersraw))
+			:dates)))
+
+(defn readfile
+	"Produces a vector like [[:date :Index1 :index 2] [date11 level11 level21] [date12 level12 level22]]"
+	([filename]
+		(with-open [rdr (io/reader (str *filepath* filename))]
+			(let 
+				[headers (readheaders (first (line-seq rdr)))
+				data (doall (map readline (line-seq rdr)))
+				cols (apply map vector data)]
+				(zipmap headers cols)))))
+
